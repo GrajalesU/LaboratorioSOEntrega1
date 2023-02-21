d802f4585bfa9938ebcac3f80a50085658370ef5
mariarua <maria.rua1@udea.edu.co>
2023-02-21 09:54:31 -0500

    Create funtion for point 6

--- array.c
+++ array.c
@@ -25,7 +25,7 @@ double illness_prob_age(items_t items, int age)
     return result;
 }
 
-int* citizens_per_city(items_t items) {
+int* citizens_per_city(items_t items) { // Solucion al ejercicio 1, reporta la cantidad de personas por cada ciudad 
     int* cities = (int*)calloc(10, sizeof(int));
 
     for(int i = 0; i < 150000; i++ ){
@@ -36,4 +36,18 @@ int* citizens_per_city(items_t items) {
 
     return cities;
 
+}
+
+int* citizens_per_age(items_t items, int age){  // Solucion al ejercicio 6, reporta la cantidad de personas por cada ciudad con X edad
+     int* cities = (int*)calloc(10, sizeof(int));
+
+    for(int i = 0; i < 150000; i++ ){
+        if(items[i].age == age){
+        cities[items[i].city]++; 
+        }
+    }
+
+    printf("%i, %i, %i, %i, %i, %i, %i, %i, %i, %i\n", cities[0], cities[1], cities[2], cities[3], cities[4], cities[5], cities[6], cities[7], cities[8], cities[9]);
+
+    return cities;
 }
