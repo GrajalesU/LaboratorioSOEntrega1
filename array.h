abf794849ef30bf6744d26dae4395f10542c6178
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-02-21 22:43:15 -0500

    Add add_item_in_middle interface

--- array.h
+++ array.h
@@ -9,4 +9,6 @@ double illness_prob_age(items_t array, int age);
 int* citizens_per_city(items_t items);
 
 int* citizens_per_age(items_t items, int age);
+
+item_t *add_item_in_middle(items_t array, int current_size, item_t item);
 #endif
