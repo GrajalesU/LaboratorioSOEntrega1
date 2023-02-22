c486e603f3207f021dab69207266942b536120f1
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-02-22 17:53:28 -0500

    Add add_item_in_middle_ll function interface

--- linked_list.h
+++ linked_list.h
@@ -12,8 +12,6 @@ void add_to_list(node_t **head, item_t item);
 
 item_t get_from_list(node_t *head, int position, int list_size);
 
-void update_in_list(node_t *head, int position, item_t new_item);
-
-void remove_from_list(node_t **head, int position);
+void add_item_in_middle_ll(node_t *head, item_t new_item);
 
 #endif
