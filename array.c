42a97d4bf7e91afccc52b6f79b0fb9add55ab64b
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-02-21 22:43:37 -0500

    Add add_item_in_middle function

--- array.c
+++ array.c
@@ -25,29 +25,59 @@ double illness_prob_age(items_t items, int age)
     return result;
 }
 
-int* citizens_per_city(items_t items) { // Solucion al ejercicio 1, reporta la cantidad de personas por cada ciudad 
-    int* cities = (int*)calloc(10, sizeof(int));
+int *citizens_per_city(items_t items)
+{ // Solucion al ejercicio 1, reporta la cantidad de personas por cada ciudad
+    int *cities = (int *)calloc(10, sizeof(int));
 
-    for(int i = 0; i < 150000; i++ ){
-        cities[items[i].city]++; 
+    for (int i = 0; i < 150000; i++)
+    {
+        cities[items[i].city]++;
     }
 
     printf("%i, %i, %i, %i, %i, %i, %i, %i, %i, %i\n", cities[0], cities[1], cities[2], cities[3], cities[4], cities[5], cities[6], cities[7], cities[8], cities[9]);
 
     return cities;
-
 }
 
-int* citizens_per_age(items_t items, int age){  // Solucion al ejercicio 6, reporta la cantidad de personas por cada ciudad con X edad
-     int* cities = (int*)calloc(10, sizeof(int));
+int *citizens_per_age(items_t items, int age)
+{ // Solucion al ejercicio 6, reporta la cantidad de personas por cada ciudad con X edad
+    int *cities = (int *)calloc(10, sizeof(int));
 
-    for(int i = 0; i < 150000; i++ ){
-        if(items[i].age == age){
-        cities[items[i].city]++; 
+    for (int i = 0; i < 150000; i++)
+    {
+        if (items[i].age == age)
+        {
+            cities[items[i].city]++;
         }
     }
 
     printf("%i, %i, %i, %i, %i, %i, %i, %i, %i, %i\n", cities[0], cities[1], cities[2], cities[3], cities[4], cities[5], cities[6], cities[7], cities[8], cities[9]);
 
     return cities;
+}
+
+item_t *add_item_in_middle(items_t array, int current_size, item_t item)
+{
+    int new_size = current_size + 1;
+    item_t *new_array = (item_t *)malloc(new_size * sizeof(item_t));
+    int half = (current_size / 2) - 1;
+    for (int i = 0; i < new_size; i++)
+    {
+        if (i < half)
+        {
+            new_array[i] = array[i];
+        }
+        else if (i == half)
+        {
+            item.id = half;
+            new_array[i] = item;
+        }
+        else
+        {
+            array[i - 1].id = i + 1;
+            new_array[i] = array[i - 1];
+        }
+    }
+
+    return new_array;
 }
